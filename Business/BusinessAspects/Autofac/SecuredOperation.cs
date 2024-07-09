ab4c2d7df5a90e463961e66af97dbd42483592a3
çağrı çamyar <cagri-95@hotmail.com>
2024-07-10 02:54:00 +0300

    Duzenlemeler yapildi Projenin Son hali

--- Business/BusinessAspects/Autofac/SecuredOperation.cs
+++ Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -3,12 +3,6 @@ using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
 using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.DynamicProxy;
 using Microsoft.Extensions.DependencyInjection;
 using Core.Extensions;
 
